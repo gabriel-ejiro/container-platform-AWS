700a25a09e464c67b86656a7dc8c9f470aea0464
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-10-18 22:34:37 +0200

    Add AWS Application Load Balancer config

---
+++ infra/alb.tf
@@ -0,0 +1,16 @@
+resource "aws_lb" "app_alb" {
+  name               = "${var.project_name}-alb"
+  load_balancer_type = "application"
+  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
+  security_groups    = [aws_security_group.alb_sg.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
