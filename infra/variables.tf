c392078685039f0d211012ac957e85b76f510e54
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-10-18 22:19:41 +0200

    Added Terraform variables for AWS config

---
+++ infra/variables.tf
@@ -0,0 +1,18 @@
+variable "region" {
+  type        = string
+  description = "AWS region"
+  default     = "eu-north-1"
+}
+
+variable "project_name" {
+  type        = string
+  default     = "demo"
+}
+
+variable "instance_type" {
+  type        = string
+  default     = "t3.micro" # or t2.micro
+}
+
+variable "github_owner" { type = string } # e.g., "gabriel-ejiro"
+variable "github_repo"  { type = string } # e.g., "container-platform"
