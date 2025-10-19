a1ca2d57ca486ba5a574586014d3fb390c33fe7c
Gabriel Ejiro <115194289+gabriel-ejiro@users.noreply.github.com>
2025-10-19 16:20:52 +0200

    Redirect HTTP traffic to HTTPS in ALB

--- infra/alb.tf
+++ infra/alb.tf
@@ -5,30 +5,33 @@ resource "aws_lb" "app_alb" {
   security_groups    = [aws_security_group.alb_sg.id]
 }
 
+# Redirect HTTP -> HTTPS
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.app_alb.arn
   port              = 80
   protocol          = "HTTP"
+
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app_tg.arn
+    type = "redirect"
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
   }
 }
 
+# HTTPS listener using your ACM cert
 resource "aws_lb_listener" "https" {
   load_balancer_arn = aws_lb.app_alb.arn
   port              = 443
   protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.app.arn  # or your issued cert ARN
+  certificate_arn   = "arn:aws:acm:eu-north-1:872926860633:certificate/e88b7813-683c-4285-b858-b222855a06be"
 
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app_tg.arn
   }
 }
-
-
-
-
 
